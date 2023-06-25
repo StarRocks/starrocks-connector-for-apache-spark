d53e02900e64bde8e11ed4b93aa54971cafcf110
PengFei Li <lpengfei2016@gmail.com>
2023-06-25 11:11:50 +0800

    [Feature] Add git-commit-id-plugin (#48)
    Signed-off-by: PengFei Li <lpengfei2016@gmail.com>

--- src/main/java/com/starrocks/connector/spark/sql/write/StarRocksWriterFactory.java
+++ src/main/java/com/starrocks/connector/spark/sql/write/StarRocksWriterFactory.java
@@ -6,8 +6,12 @@ import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.connector.write.DataWriter;
 import org.apache.spark.sql.connector.write.DataWriterFactory;
 import org.apache.spark.sql.connector.write.streaming.StreamingDataWriterFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StarRocksWriterFactory implements DataWriterFactory, StreamingDataWriterFactory {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StarRocksWriterFactory.class);
 
     private final RowStringConverter converter;
     private final WriteStarRocksConfig config;
@@ -19,11 +23,30 @@ public class StarRocksWriterFactory implements DataWriterFactory, StreamingDataW
 
     @Override
     public DataWriter<InternalRow> createWriter(int partitionId, long taskId) {
-        return new StarRocksDataWriter(config, converter, partitionId, taskId, -1);
+        return createAndOpenWriter(partitionId, taskId, -1);
     }
 
     @Override
     public DataWriter<InternalRow> createWriter(int partitionId, long taskId, long epochId) {
-        return new StarRocksDataWriter(config, converter, partitionId, taskId, epochId);
+        return createAndOpenWriter(partitionId, taskId, epochId);
+    }
+
+    private StarRocksDataWriter createAndOpenWriter(int partitionId, long taskId, long epochId) {
+        StarRocksDataWriter writer = new StarRocksDataWriter(config, converter, partitionId, taskId, epochId);
+        try {
+            writer.open();
+        } catch (Exception e) {
+            String errMsg = String.format("Failed to open writer for " +
+                            "partition: %s, task: %s, epoch: %s", partitionId, taskId, epochId);
+            LOG.error("{}", errMsg, e);
+            try {
+                writer.close();
+            } catch (Exception ce) {
+                LOG.error("Failed to close writer for partition: {}, task: {}, epoch: {}", partitionId, taskId, epochId, ce);
+            }
+
+            throw new RuntimeException(errMsg, e);
+        }
+        return writer;
     }
 }
